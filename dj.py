ca4a3cd70c445a1c9f851ed6116e1a542c592552
luc1ferSemerninov <semerninov000@gmail.com>
2023-09-07 18:27:05 +0600

    Более менее рабочий код

---
+++ dj.py
@@ -0,0 +1,24 @@
+# a=['yabloko','banan','jokolad']
+# print(a)
+# print(type(a))
+# print(len(a))
+# b = {'key':a}
+# print(b)
+# print(type(b))
+# print(len(b))
+
+# d = 12
+# c = [b,a,"123", 123454, d]
+# print(c)
+# print(type(c))
+# print(len(c))
+
+# c0 = c[0] 
+# print(c0)
+# print(type(c0))
+# print(len(c0))
+
+
+tasklist = ['1','Первая задача','Готово','Комментарий']
+
+jojo = {'taskid':1, 'taskname':"Name", 'result':"Готво", 'comm':"Коммент"}
